60bf280f5390b2bff170ff99048103fc24f1da33
Davide Busolin <davide.busolin@studenti.unimi.it>
2023-12-12 11:28:08 +0100

    profiling

--- zero-delay-network/main.py
+++ zero-delay-network/main.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import zmq
+import cProfile, pstats
 
 
 def send_info(socket, t):
@@ -10,74 +11,81 @@ def send_info(socket, t):
 def receive_info(socket):
     message = socket.recv()
     json_data = json.loads(message.decode("utf-8"))
-    if json_data['simulation_status'] != 0:
-        sys.exit(0)
+    #if json_data['simulation_status'] != 0:
+    #    sys.exit(0)
     return json_data
-
+    
 
 if __name__ == '__main__':
-    refresh_status = 0.01
-    simulation_step = 0.01
+    with cProfile.Profile() as profile:
+        refresh_status = 0.01 #0.04 #25fps?
+        simulation_step = 0.01
 
 
-    def read_json(type_request):
-        with open(f'API/{type_request}.json') as f:
-            return json.load(f)
+        def read_json(type_request):
+            with open(f'API/{type_request}.json') as f:
+                return json.load(f)
 
 
-    init_configuration_json = read_json('init')
+        init_configuration_json = read_json('init')
 
-    interested_actor = init_configuration_json['moving_actors'][0]
-    actor_id = interested_actor['actor_id']
-    agent_id = interested_actor['actor_configuration']['agent_id']
+        interested_actor = init_configuration_json['moving_actors'][0]
+        actor_id = interested_actor['actor_id']
+        agent_id = interested_actor['actor_configuration']['agent_id']
 
-    if len(sys.argv) >= 2:
-        init_configuration_json['carla_configuration']['seed'] = int(sys.argv[1])
-        init_configuration_json['run_id'] +=  f"#{init_configuration_json['carla_configuration']['seed']}"
+        if len(sys.argv) >= 2:
+            init_configuration_json['carla_configuration']['seed'] = int(sys.argv[1])
+            init_configuration_json['run_id'] +=  f"#{init_configuration_json['carla_configuration']['seed']}"
 
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    #socket.connect("tcp://tod-carla:5555")
-    socket.connect("tcp://localhost:5555")
-    print("connected")
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        #socket.connect("tcp://tod-carla:5555")
+        socket.connect("tcp://localhost:5555")
+        print("connected")
 
-    send_info(socket, init_configuration_json)
-    message = receive_info(socket)
-    timestamp = message['initial_timestamp']
-    limit_sim_time = 15
+        send_info(socket, init_configuration_json)
+        message = receive_info(socket)
+        timestamp = message['initial_timestamp']
+        limit_sim_time = 15
 
-    while True:
-        for _ in range(int(refresh_status / simulation_step)):
-            timestamp += simulation_step
-            req = read_json('simulation_step')
+        while True:
+            #if timestamp >= 1.1: break #########TEST TEST TEST
+            for _ in range(int(refresh_status / simulation_step)):
+                timestamp += simulation_step
+                req = read_json('simulation_step')
+                req['timestamp'] = timestamp
+                send_info(socket, req)
+                message = receive_info(socket)
+                if message['simulation_status'] != 0: break
+            if message['simulation_status'] != 0: break
+
+            req = read_json('actor_status_update')
+            req['timestamp'] = timestamp
+            send_info(socket, req)
+
+            message = receive_info(socket)
+            if message['simulation_status'] != 0: break
+            status_id = message['user_defined']['status_id']
+
+            req = read_json('compute_instruction')
+            req['user_defined']['status_id'] = status_id
+            req['timestamp'] = timestamp
+            send_info(socket, req)
+
+            message = receive_info(socket)
+            if message['simulation_status'] != 0:
+                break
+            instruction_id = message['user_defined']['instruction_id']
+
+            req = read_json('apply_instruction')
+            req['user_defined']['instruction_id'] = instruction_id
             req['timestamp'] = timestamp
             send_info(socket, req)
             message = receive_info(socket)
-            if message['simulation_status'] != 0: break
-        if message['simulation_status'] != 0: break
-
-        req = read_json('actor_status_update')
-        req['timestamp'] = timestamp
-        send_info(socket, req)
-
-        message = receive_info(socket)
-        if message['simulation_status'] != 0: break
-        status_id = message['user_defined']['status_id']
-
-        req = read_json('compute_instruction')
-        req['user_defined']['status_id'] = status_id
-        req['timestamp'] = timestamp
-        send_info(socket, req)
-
-        message = receive_info(socket)
-        if message['simulation_status'] != 0:
-            break
-        instruction_id = message['user_defined']['instruction_id']
-
-        req = read_json('apply_instruction')
-        req['user_defined']['instruction_id'] = instruction_id
-        req['timestamp'] = timestamp
-        send_info(socket, req)
-        message = receive_info(socket)
-        if message['simulation_status'] != 0:
-            break
+            if message['simulation_status'] != 0:
+                break
+        
+        results = pstats.Stats(profile)
+        results.sort_stats(pstats.SortKey.TIME)
+        results.print_stats()
+        results.dump_stats("results_zerodelay.prof")
